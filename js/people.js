b3ce7576727a3cf9243f76b6ab72a449cc2121b2
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-09-21 15:26:41 -0500

    planets ok

--- js/people.js
+++ js/people.js
@@ -1,63 +1,68 @@
 
 import { showGlobalLoading, hideGlobalLoading } from './app.js';
 let currentPage = 1;
-const prev = document.getElementById("prev");
-const next = document.getElementById("next");
-const span = document.getElementById("page");
+const pagination = {
+  prev: document.getElementById("prev"),
+  next: document.getElementById("next"),
+  span: document.getElementById("page"),
+}
 
 function nextPage() {
-  currentPage++;
+  currentPage += 1;
   fetchPeople(currentPage);
-  span.textContent = currentPage;
+  pagination.span.textContent = currentPage;
 }
 
 function prevPage() {
   if (currentPage > 1) {
-    currentPage--;
-    span.textContent = currentPage;
+    currentPage -= 1;
+    pagination.span.textContent = currentPage;
     fetchPeople(currentPage);
   }
 }
 
-prev.addEventListener("click", prevPage);
+pagination.prev.addEventListener("click", prevPage);
 
-next.addEventListener("click", nextPage);
+pagination.next.addEventListener("click", nextPage);
 
 function fetchPeople(page) {
-  const peopleList = document.getElementById("peopleList");
-  const modal = document.getElementById("myModal");
-  const closeButton = document.getElementById("close");
-  const apiUrl = `https://swapi.dev/api/people/?page=${page}`;
+  const elements = {
+    peopleList: document.getElementById("peopleList"),
+    modal : document.getElementById("myModal"),
+    closeButton : document.getElementById("close"),
+    apiUrl : `https://swapi.dev/api/people/?page=${page}`,
 
-  const headerModal = document.getElementById("header");
-  const birth_year = document.getElementById("birth_year");
-  const eye_color = document.getElementById("eye_color");
-  const gender = document.getElementById("gender");
-  const hair_color = document.getElementById("hair_color");
-  const height = document.getElementById("height");
-  const homeworld = document.getElementById("homeworld");
-  const mass = document.getElementById("mass");
-  const skin = document.getElementById("skin");
+    headerModal : document.getElementById("header"),
+    birth_year : document.getElementById("birth_year"),
+    eye_color : document.getElementById("eye_color"),
+    gender : document.getElementById("gender"),
+    hair_color : document.getElementById("hair_color"),
+    height : document.getElementById("height"),
+    homeworld : document.getElementById("homeworld"),
+    mass : document.getElementById("mass"),
+    skin : document.getElementById("skin"),
+  }
   
-  closeButton.addEventListener("click", () => {
-    modal.style.display = "none";
+  elements.closeButton.addEventListener("click", () => {
+    elements.modal.style.display = "none";
   });
   
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
-      modal.style.display = "none";
+  elements.modal.addEventListener("click", (e) => {// cierre de modal al dar click fuerA DE EL
+    if (e.target === elements.modal) {
+      elements.modal.style.display = "none";
     }
   });
   
   showGlobalLoading();
 
-  fetch(apiUrl)
+  fetch(elements.apiUrl)
     .then((response) => response.json())
     .then((data) => {
       
-      console.log(data);
-      span.textContent = currentPage;
-      peopleList.innerHTML = "";
+      console.log(data, currentPage);
+      pagination.span.textContent = currentPage;
+      elements.peopleList.innerHTML = "";
+      
       data.results.forEach((people) => {
         const row = document.createElement("li");
         row.classList.add("people__item");
@@ -67,19 +72,18 @@ function fetchPeople(page) {
         button.classList.add("people__btn");
         button.addEventListener("click", () => {
           
-        modal.style.display = "flex";
-        
-        headerModal.textContent = people.name;
-        birth_year.textContent = people.birth_year;
-        eye_color.textContent = people.eye_color;
-        gender.textContent = people.gender;
-        hair_color.textContent = people.hair_color;
-        height.textContent = people.height + " Cm";
+        elements.modal.style.display = "flex";
+        elements.headerModal.textContent = people.name;
+        elements.birth_year.textContent = people.birth_year;
+        elements.eye_color.textContent = people.eye_color;
+        elements.gender.textContent = people.gender;
+        elements.hair_color.textContent = people.hair_color;
+        elements.height.textContent = people.height + " Cm";
         const startIndex = people.homeworld.indexOf("planets/");
         const result = people.homeworld.substring(startIndex);
-        homeworld.textContent = result;
-        mass.textContent = people.mass;
-        skin.textContent = people.skin_color;
+        elements.homeworld.textContent = result;
+        elements.mass.textContent = people.mass;
+        elements.skin.textContent = people.skin_color;
 
         if (!people.species || people.species.length === 0) {
           console.log("Sin species");
@@ -104,51 +108,51 @@ function fetchPeople(page) {
             });
           }
 
-          if (!people.starships || people.starships.length === 0) {
-            console.log("Sin species");
-          } else {
-            const starships = document.getElementById("starships");
-            starships.innerHTML = "";
-            people.starships.map((starship) => {
-              fetch(starship)
-                .then(response => response.json())
-                .then(starshipsData => {
-                  const option = document.createElement("option");
-                  option.classList.add("modal__option");
-                  option.value = starshipsData.name; 
-                  option.text = starshipsData.name; 
-                  starships.appendChild(option);
-                })
-                .catch(error => {
-                  console.error("Error al obtener datos de la vehicles:", error);
-                });
-            });
-          }
+          // if (!people.starships || people.starships.length === 0) {
+          //   console.log("Sin species");
+          // } else {
+          //   const starships = document.getElementById("starships");
+          //   starships.innerHTML = "";
+          //   people.starships.map((starship) => {
+          //     fetch(starship)
+          //       .then(response => response.json())
+          //       .then(starshipsData => {
+          //         const option = document.createElement("option");
+          //         option.classList.add("modal__option");
+          //         option.value = starshipsData.name; 
+          //         option.text = starshipsData.name; 
+          //         starships.appendChild(option);
+          //       })
+          //       .catch(error => {
+          //         console.error("Error al obtener datos de la vehicles:", error);
+          //       });
+          //   });
+          // }
 
-          if (!people.vehicles || people.vehicles.length === 0) {
-            console.log("Sin species");
-          } else {
-            const vehicles = document.getElementById("vehicles");
-            vehicles.innerHTML = "";
-            people.vehicles.map((vehicle) => {
-              fetch(vehicle)
-                .then(response => response.json())
-                .then(vehiclesData => {
-                  const option = document.createElement("option");
-                  option.classList.add("modal__option");
-                  option.value = vehiclesData.name; 
-                  option.text = vehiclesData.name; 
-                  vehicles.appendChild(option);
-                })
-                .catch(error => {
-                  console.error("Error al obtener datos de la vehicles:", error);
-                });
-            });
-          }
+          // if (!people.vehicles || people.vehicles.length === 0) {
+          //   console.log("Sin species");
+          // } else {
+          //   const vehicles = document.getElementById("vehicles");
+          //   vehicles.innerHTML = "";
+          //   people.vehicles.map((vehicle) => {
+          //     fetch(vehicle)
+          //       .then(response => response.json())
+          //       .then(vehiclesData => {
+          //         const option = document.createElement("option");
+          //         option.classList.add("modal__option");
+          //         option.value = vehiclesData.name; 
+          //         option.text = vehiclesData.name; 
+          //         vehicles.appendChild(option);
+          //       })
+          //       .catch(error => {
+          //         console.error("Error al obtener datos de la vehicles:", error);
+          //       });
+          //   });
+          // }
 
         });
         row.appendChild(button);
-        peopleList.appendChild(row);
+        elements.peopleList.appendChild(row);
       });
       hideGlobalLoading();
     })
